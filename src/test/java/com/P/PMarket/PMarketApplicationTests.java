19b1a667d418539441333e72a2b01da2e19c13a0
FGerbes <101647469+FGerbes@users.noreply.github.com>
2022-11-02 09:51:18 +0100

    Init

---
+++ src/test/java/com/P/PMarket/PMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P.PMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
