d1ef944b471bb1ff75167341c605a557ca84cd97
FGerbes <101647469+FGerbes@users.noreply.github.com>
2022-11-05 14:39:53 +0100

    serverRunning

---
+++ src/main/generated/com/P/PMarket/persistence/mapper/CategoryMapperImpl.java
@@ -0,0 +1,49 @@
+package com.P.PMarket.persistence.mapper;
+
+import com.P.PMarket.domain.Category;
+import com.P.PMarket.persistence.entity.Categoria;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-11-05T13:34:10+0100",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.17 (Amazon.com Inc.)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(Categoria categoria) {
+        if ( categoria == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        if ( categoria.getIdCategoria() != null ) {
+            category.setCategoryId( categoria.getIdCategoria() );
+        }
+        category.setCategory( categoria.getDescripcion() );
+        if ( categoria.getEstado() != null ) {
+            category.setActive( categoria.getEstado() );
+        }
+
+        return category;
+    }
+
+    @Override
+    public Categoria toCategoria(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Categoria categoria = new Categoria();
+
+        categoria.setIdCategoria( category.getCategoryId() );
+        categoria.setDescripcion( category.getCategory() );
+        categoria.setEstado( category.isActive() );
+
+        return categoria;
+    }
+}
---
+++ src/main/java/com/P/PMarket/domain/Category.java
@@ -0,0 +1,10 @@
+package com.P.PMarket.domain;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private int categoryId;
+    private String category;
+    private boolean active;
+}
