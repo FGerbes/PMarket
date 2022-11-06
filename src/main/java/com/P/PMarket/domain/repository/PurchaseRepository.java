2837cad3a293c190668ede8b8e58631fb2b1715c
FGerbes <101647469+FGerbes@users.noreply.github.com>
2022-11-06 13:42:58 +0100

    Working2

---
+++ src/main/java/com/P/PMarket/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.P.PMarket.domain.repository;
+
+import com.P.PMarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
